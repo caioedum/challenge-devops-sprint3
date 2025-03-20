7cb1c88a715ce93177af919be4e0ed14c2f1efaf
Caio Eduardo <caio3martins@gmail.com>
2025-03-19 23:26:27 -0300

    fix: corrige o roteamento do swagger

--- WebApiChallenge/Program.cs
+++ WebApiChallenge/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,7 +12,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add DbContext string connection
-
 builder.Services.AddSwaggerGen(c =>
 {
     c.EnableAnnotations();
@@ -23,11 +20,13 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configura o Swagger para exibir na URL raiz
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Exibe Swagger na URL raiz
+});
 
 app.UseHttpsRedirection();
 
